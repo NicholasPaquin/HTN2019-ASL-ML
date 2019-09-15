56f3eee0ecc6744838657571a453321a7d994f9d
Jeffrey-ch-Ng <ng.jeffrey@rogers.com>
2019-09-15 09:15:17 -0400

    update flask

--- flask/app.py
+++ flask/app.py
@@ -3,15 +3,25 @@ from machine_learning.model import return_hello
 from flask import Flask
 from flask import request, Response
 import json
+import base64
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
-        data = request.get_json()
-        print(data)
-        return json.dumps(data)
+        encodedMessage = request.get_json()
+        print(encodedMessage)
+
+        byteArray = encodedMessage['byteArray']
+
+        print(byteArray)
+
+        decodedMessage = base64.b64decode(byteArray)
+
+        # send decoded model, return data
+
+        return decodedMessage
     
     elif request.method == 'GET':
         return_hello()
